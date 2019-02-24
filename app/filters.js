8f0200c7f98e1396cac41088c961835419187154
christinagyles_ea <christina.gyles@environment-agency.gov.uk>
2019-02-24 21:07:44 +0000

    save or resume pages

--- app/filters.js
+++ app/filters.js
@@ -37,7 +37,29 @@ module.exports = function (env) {
     documentation.
 
   ------------------------------------------------------------------ */
+  filters.nl2br = function(textStr) {
+    return textStr.replace(/\r|\n|\r\n/g, '<br />')
+}
 
+filters.typeof = function(textStr) {
+    return typeof textStr
+}
+
+filters.formatnumber = function(num) {
+    var formattedNum = num.toLocaleString('en',{ style: 'currency', currency: 'GBP' })
+    var returnNum = formattedNum
+    if(formattedNum.endsWith(".00")) returnNum = formattedNum.substring( 0, (formattedNum.length-3) )
+    return returnNum
+}
+
+filters.reverseName = function(textStr) {
+   var parts = textStr.split(',')
+   if(parts[1]===undefined){
+     return parts[0]
+   } else {
+     return parts[1] + ' ' + parts[0]
+   }
+ }
   /* ------------------------------------------------------------------
     keep the following line to return your filters to the app
   ------------------------------------------------------------------ */
--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,42 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.use(function (req, res, next) {
+    res.locals.hostname=process.env.THISHOST
+    next()
+  });
+
+// anything beginning with "/vn" will go into this
+//router.use('/v1', require('./routes_v1'))
+//router.use('/v2', require('./routes_v2'))
+//router.use('/v2a', require('./routes_v2a'))
+//router.use('/v3', require('./routes_v3'))
+//router.use('/v4', require('./routes_v4'))
+//router.use('/v5', require('./routes_v5'))
+//router.use('/v6', require('./routes_v6'))
+//router.use('/v7', require('./routes_v7'))
+//router.use('/v8', require('./routes_v8'))
+//router.use('/v9', require('./routes_v9'))
+//router.use('/v10', require('./routes_v10'))
+//router.use('/v11', require('./routes_v11'))
+//router.use('/v12', require('./routes_v12'))
+//router.use('/v13', require('./routes_v13'))
+//router.use('/v14', require('./routes_v14'))
+//router.use('/v15', require('./routes_v15'))
+//router.use('/v16', require('./routes_v16'))
+//router.use('/v17', require('./routes_v17'))
+//router.use('/v18', require('./routes_v18'))
+//router.use('/v19', require('./routes_v19'))
+//router.use('/v20', require('./routes_v20'))
+//router.use('/v21', require('./routes_v21'))
+//router.use('/v22', require('./routes_v22'))
+//router.use('/v23', require('./routes_v23'))
+//router.use('/v24', require('./routes_v24'))
+
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
 module.exports = router
